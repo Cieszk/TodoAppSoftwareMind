719e28b05629d7b5a2fa39a9b50fcbff87aed705
cieszk <cieszkens@gmail.com>
2023-07-24 18:15:11 +0200

    project init

---
+++ src/test/java/pl/cieszk/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cieszk.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
